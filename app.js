73b3ea7740fe7401b5db91bd2ff2c7f4773a9d64
miju462.park <miju462.park@hanwha.com>
2018-12-20 16:34:16 +0900

    [Routing] split routing for /user

--- app.js
+++ app.js
@@ -3,13 +3,9 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
+import {userRouter} from './router.js';
 
 const app = express();
-
-const PORT = 4000;
-
-const handleListening = () =>
-  console.log(`Listening on :http://localhost:${PORT}`);
 
 const handleHome = (req, res) => res.send("Hello from my ass");
 
@@ -20,8 +16,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(helmet());
 app.use(morgan("dev"));
+app.use("/user", userRouter);
 
 app.get('/', handleHome);
 app.get('/profile', handleProfile);
 
-app.listen(PORT, handleListening);
+export default app;
