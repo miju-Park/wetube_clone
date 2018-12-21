16b7432ed16d0a813ba360290e8abfaa9094400c
miju462.park <miju462.park@hanwha.com>
2018-12-21 15:52:38 +0900

    Change Profile HTML

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,12 @@
 export const join = (req, res) => res.render("join", {pageTitle: "Join"});
 export const login = (req, res) => res.render("login", {pageTitle: "Login"});
 export const logout = (req, res) => res.render("logout", {pageTitle: "Logout"});
-export const userDetail = (req, res) => res.render("userDetail", {pageTitle: "User Detail"});
-export const editProfile = (req, res) => res.render("editProfile", {pageTitle: "Edit Profile"});
+export const userDetail = (req, res) => {
+  res.render("userDetail", {pageTitle: "User Detail"});
+  console.log("?");
+}
+export const editProfile = (req, res) => {
+  res.render("editProfile", {pageTitle: "Edit Profile"});
+  console.log("editProfile");
+}
 export const changePassword = (req, res) => res.render("changePassword", {pageTitle: "Change Password"});
--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,7 +1,6 @@
 import express from "express";
 import routes from "../routes";
 import {
-  users, 
   userDetail, 
   editProfile, 
   changePassword
@@ -9,9 +8,13 @@ import {
 
 const userRouter = express.Router();
 
-userRouter.get(routes.userDetail, userDetail);
+console.log(userDetail);
+console.log(editProfile);
+
+
+console.log(routes.editProfile);
 userRouter.get(routes.editProfile, editProfile);
 userRouter.get(routes.changePassword, changePassword);
-
+userRouter.get(routes.userDetail, userDetail);
 
 export default userRouter;
